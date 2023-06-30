5aaeba108b2b11eb15bc6bb73c84a0c83eb1e08b
Richard Calvin Yong <r.calvin.yong@gmail.com>
2023-06-30 22:35:42 +0700

    Added basic Home & Login page with Routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,29 @@
 import { useState } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import "./App.css";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import Layout from "./components/Layout";
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/login",
+        element: <LoginPage />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <>
-      <h1>Hello, World</h1>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
