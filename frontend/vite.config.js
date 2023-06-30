832062ff1c92a5153fe8b7abffb86f4f7bb407f9
Richard Calvin Yong <r.calvin.yong@gmail.com>
2023-06-30 21:03:14 +0700

    Created & Configured Vite React App

---
+++ frontend/vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      // https://stackoverflow.com/questions/64677212/how-to-configure-proxy-in-vite
+      "/api": {
+        target: "http://127.0.0.1:8000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
