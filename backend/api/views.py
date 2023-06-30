01344a27c6d261d1debe6b9d7339478782f4a27b
Richard Calvin Yong <r.calvin.yong@gmail.com>
2023-06-30 11:07:29 +0700

    Added Django Rest Framework

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 # Create your views here.
 
 
+@api_view(["GET"])
 def get_routes(request):
     routes = [
         "TBD",
     ]
 
-    return JsonResponse(routes, safe=False)
+    return Response(routes)
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # API App
     "api",
+    # Additional Libraries
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
