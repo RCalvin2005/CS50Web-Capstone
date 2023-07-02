ff7c53d2dfa93fd0464bb0b150ad47398a934ac2
Richard Calvin Yong <r.calvin.yong@gmail.com>
2023-07-02 21:47:05 +0700

    Created example protected endpoint using test model Note

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = "api"
 urlpatterns = [
     path("", views.get_routes, name="get_routes"),
+    path("notes/", views.get_notes, name="get_notes"),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,9 @@
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+
+from .serializers import NoteSerializer
+from .models import Note
 
 # Create your views here.
 
@@ -9,5 +13,13 @@ def get_routes(request):
     routes = [
         "TBD",
     ]
-
     return Response(routes)
+
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def get_notes(request):
+    user = request.user
+    notes = user.note_set.all()
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)
