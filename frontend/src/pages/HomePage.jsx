bd13dffc60488a9ee7c734d79f774befa2f500d0
Richard Calvin Yong <r.calvin.yong@gmail.com>
2023-07-02 22:18:23 +0700

    Created example protected endpoint fetch request

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,40 @@
-import React from "react";
+import { useState, useEffect, useContext } from "react";
+import AuthContext from "../context/AuthContext";
 
 function HomePage() {
+  const [notes, setNotes] = useState([]);
+  const { authTokens, logoutUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    getNotes();
+  }, []);
+
+  async function getNotes() {
+    let response = await fetch("/api/notes/", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + String(authTokens.access),
+      },
+    });
+    let data = await response.json();
+
+    if (response.status === 200) {
+      setNotes(data);
+    } else if (response.statusText === "Unauthorized") {
+      logoutUser();
+    }
+  }
+
   return (
     <div>
       <p>Welcome to the home page!</p>
+
+      <ul>
+        {notes.map((note) => (
+          <li key={note.id}>{note.body}</li>
+        ))}
+      </ul>
     </div>
   );
 }
