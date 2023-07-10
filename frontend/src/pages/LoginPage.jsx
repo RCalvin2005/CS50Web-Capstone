7ee25fcd00d3794b9d0a3ffe91c5d353d444348f
Richard Calvin Yong <r.calvin.yong@gmail.com>
2023-07-10 12:06:25 +0700

    Redesigned LoginPage using MUI

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,17 +1,43 @@
 import React, { useContext } from "react";
 import AuthContext from "../context/AuthContext";
+import { Box, Button, Card, TextField } from "@mui/material";
 
 function LoginPage() {
   const { loginUser } = useContext(AuthContext);
 
   return (
-    <div>
-      <form onSubmit={loginUser}>
-        <input type="text" name="username" placeholder="Enter Username" />
-        <input type="password" name="password" placeholder="Enter Password" />
-        <input type="submit" />
-      </form>
-    </div>
+    <Box display="flex" justifyContent="center">
+      <Card
+        sx={{
+          maxWidth: { xs: 350, sm: 400 },
+          padding: { xs: "30px 25px", sm: "30px 40px" },
+        }}
+        elevation={3}
+      >
+        <form onSubmit={loginUser}>
+          <TextField
+            name="username"
+            label="Username"
+            placeholder="Enter Username"
+            variant="outlined"
+            margin="normal"
+            fullWidth
+          />
+          <TextField
+            name="password"
+            type="password"
+            label="Password"
+            placeholder="Enter Password"
+            variant="outlined"
+            margin="normal"
+            fullWidth
+          />
+          <Button type="submit" variant="contained" sx={{ mt: 1 }} fullWidth>
+            Sign In
+          </Button>
+        </form>
+      </Card>
+    </Box>
   );
 }
 
