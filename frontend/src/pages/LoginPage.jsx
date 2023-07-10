4acb9dfb7e74805008ce4111fd5074386dbaf191
Richard Calvin Yong <r.calvin.yong@gmail.com>
2023-07-10 21:46:51 +0700

    Added password visiblility toggle & header

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,19 +1,52 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import AuthContext from "../context/AuthContext";
-import { Box, Button, Card, TextField } from "@mui/material";
+import {
+  Box,
+  Button,
+  Card,
+  Divider,
+  FormControl,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { EditCalendar, Visibility, VisibilityOff } from "@mui/icons-material";
 
 function LoginPage() {
   const { loginUser } = useContext(AuthContext);
+  const [showPassword, setShowPassword] = useState(false);
 
   return (
-    <Box display="flex" justifyContent="center">
+    <Box display="flex" justifyContent="center" mt={{ xs: 12, sm: 6 }}>
       <Card
         sx={{
-          maxWidth: { xs: 350, sm: 400 },
+          maxWidth: { xs: 335, sm: 400 },
           padding: { xs: "30px 25px", sm: "30px 40px" },
         }}
         elevation={3}
       >
+        <Box display="flex" justifyContent="center" gap={1} py={2}>
+          <Typography component="h2" variant="h4" align="center">
+            Course Planner
+          </Typography>
+          <EditCalendar fontSize="large" />
+        </Box>
+
+        <Divider />
+
+        <Typography
+          component="h3"
+          variant="h5"
+          fontWeight={500}
+          align="center"
+          sx={{ mt: 3, mb: 1 }}
+        >
+          Login
+        </Typography>
+
         <form onSubmit={loginUser}>
           <TextField
             name="username"
@@ -23,16 +56,31 @@ function LoginPage() {
             margin="normal"
             fullWidth
           />
-          <TextField
-            name="password"
-            type="password"
-            label="Password"
-            placeholder="Enter Password"
-            variant="outlined"
-            margin="normal"
-            fullWidth
-          />
-          <Button type="submit" variant="contained" sx={{ mt: 1 }} fullWidth>
+          <FormControl variant="outlined" margin="normal" fullWidth>
+            <InputLabel htmlFor="password-input">Password</InputLabel>
+            <OutlinedInput
+              name="password"
+              id="password-input"
+              placeholder="Enter Password"
+              label="Password"
+              type={showPassword ? "text" : "password"}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={() => setShowPassword((show) => !show)}
+                    onMouseDown={(e) => {
+                      e.preventDefault();
+                    }}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+            />
+          </FormControl>
+          <Button type="submit" variant="contained" sx={{ my: 3 }} fullWidth>
             Sign In
           </Button>
         </form>
